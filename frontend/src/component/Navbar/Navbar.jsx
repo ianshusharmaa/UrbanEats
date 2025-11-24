2bc1ac3ae281f5f9f7ecf179984c472a83be85ce
Anshu Sharma <anshusharma4165@gmail.com>
2025-11-24 19:54:04 +0530

    navbar structure and css styling

--- frontend/src/component/Navbar/Navbar.jsx
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -4,10 +4,25 @@ import { assets } from '../../assets/assets/frontend_assets/assets'
 
 
 
+
 function Navbar() {
     return (
         <div className='navbar'>
-            <img src={assets.logo} alt="" srcset="" />
+            <img src={assets.logo} alt="" className='logo' />
+            <ul className="navbar-menu">
+                <li>home</li>
+                <li>menu</li>
+                <li>mobile-app</li>
+                <li>contact us </li>
+            </ul>
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="" />
+                <div className="navbar-scearch-icon">
+                    <img src={assets.basket_icon} alt="" />
+                    <div className="dot"></div>
+                </div>
+                <button>sign in</button>
+            </div>
         </div>
     )
 }
